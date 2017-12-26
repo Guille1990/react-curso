abfd16b121923c5350c5707feef0583c8adbb5b6
DOM_BCI_PROD\exguill <gllancaq@everis.com>
2017-12-26 12:31:58 -0300

    configuración de webpack terminada

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const container = document.getElementById('container')
+const holaMundo = <h1>Hola mundo!</h1>
+
+// ReactDOM.render(que voy a renderizar, donde se va a renderizar)
+ReactDOM.render(holaMundo, container)
